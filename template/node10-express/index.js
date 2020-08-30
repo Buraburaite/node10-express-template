b0c1288df4337ab54a695fd8b8289b499a96ecea
Buraburaite <jvrcueto@outlook.com>
2020-08-30 14:25:20 -0400

    Export unbound server for unit testing
    Purpose:
    Running `faas up` every time a change is made to a function is very slow.
    Exporting unbound server instances allows unit testing frameworks to test functions quickly and in parallel.
    Any issues found after `faas up` must then be related to the build process or the environment, which makes debugging simpler as well.
    
    Changes:
    - `app` is wrapped in a function called `createApp`.
    - `app.listen` occurs in a `require.main === module` block, so that port binding only occurs if index.js is the entrypoint (called directly by node).
    - `createApp` is exported.
    - minor style changes

--- template/node10-express/index.js
+++ template/node10-express/index.js
@@ -3,104 +3,106 @@
 
 "use strict"
 
-const express = require('express')
-const app = express()
-const handler = require('./function/handler');
-const bodyParser = require('body-parser')
+class FunctionEvent {
+  constructor(req) {
+    this.body    = req.body;
+    this.headers = req.headers;
+    this.method  = req.method;
+    this.query   = req.query;
+    this.path    = req.path;
+  }
+}
 
-if (process.env.RAW_BODY === 'true') {
+class FunctionContext {
+  constructor(cb) {
+    this.value = 200;
+    this.cb = cb;
+    this.headerValues = {};
+  }
+
+  status(value) {
+    if (!value) {
+      return this.value;
+    }
+
+    this.value = value;
+    return this;
+  }
+
+  headers(value) {
+    if (!value) {
+      return this.headerValues;
+    }
+
+    this.headerValues = value;
+    return this;
+  }
+
+  succeed(value) {
+    let err;
+    this.cb(err, value);
+  }
+
+  fail(value) {
+    let message;
+    this.cb(value, message);
+  }
+}
+
+const isArray  = x => (!!x) && (x.constructor === Array );
+const isObject = x => (!!x) && (x.constructor === Object);
+const createApp = () => {
+  const app = require('express')();
+  const handler = require('./function/handler');
+  const bodyParser = require('body-parser');
+
+  if (process.env.RAW_BODY === 'true') {
     app.use(bodyParser.raw({ type: '*/*' }))
-} else {
+  } else {
     var jsonLimit = process.env.MAX_JSON_SIZE || '100kb' //body-parser default
     app.use(bodyParser.json({ limit: jsonLimit}));
     app.use(bodyParser.raw()); // "Content-Type: application/octet-stream"
     app.use(bodyParser.text({ type : "text/*" }));
-}
+  }
 
-app.disable('x-powered-by');
+  app.disable('x-powered-by');
 
-class FunctionEvent {
-    constructor(req) {
-        this.body = req.body;
-        this.headers = req.headers;
-        this.method = req.method;
-        this.query = req.query;
-        this.path = req.path;
-    }
-}
-
-class FunctionContext {
-    constructor(cb) {
-        this.value = 200;
-        this.cb = cb;
-        this.headerValues = {};
-    }
-
-    status(value) {
-        if(!value) {
-            return this.value;
-        }
-
-        this.value = value;
-        return this;
-    }
-
-    headers(value) {
-        if(!value) {
-            return this.headerValues;
-        }
-
-        this.headerValues = value;
-        return this;    
-    }
-
-    succeed(value) {
-        let err;
-        this.cb(err, value);
-    }
-
-    fail(value) {
-        let message;
-        this.cb(value, message);
-    }
-}
-
-var middleware = (req, res) => {
+  let middleware = (req, res) => {
     let cb = (err, functionResult) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send(err);
-        }
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
 
-        if(isArray(functionResult) || isObject(functionResult)) {
-            res.set(fnContext.headers()).status(fnContext.status()).send(JSON.stringify(functionResult));
-        } else {
-            res.set(fnContext.headers()).status(fnContext.status()).send(functionResult);
-        }
+      if (isArray(functionResult) || isObject(functionResult)) {
+        res.set(fnContext.headers()).status(fnContext.status()).send(JSON.stringify(functionResult));
+      } else {
+        res.set(fnContext.headers()).status(fnContext.status()).send(functionResult);
+      }
     };
 
     let fnEvent = new FunctionEvent(req);
     let fnContext = new FunctionContext(cb);
 
     handler(fnEvent, fnContext, cb);
-};
+  };
 
-app.post('/*', middleware);
-app.get('/*', middleware);
-app.patch('/*', middleware);
-app.put('/*', middleware);
-app.delete('/*', middleware);
+  app.post  ('/*', middleware);
+  app.get   ('/*', middleware);
+  app.patch ('/*', middleware);
+  app.put   ('/*', middleware);
+  app.delete('/*', middleware);
 
-const port = process.env.http_port || 3000;
+  return app;
+}
 
-app.listen(port, () => {
+// exporting createApp enables creation of multiple server instances for unit testing
+module.exports = createApp;
+
+// server binds to port only if this file is the entrypoint (i.e. 'node index.js')
+if (require.main === module) {
+  const port = process.env.http_port || 3000;
+  createApp().listen(port, () => {
     console.log(`OpenFaaS Node.js listening on port: ${port}`)
-});
-
-let isArray = (a) => {
-    return (!!a) && (a.constructor === Array);
-};
-
-let isObject = (a) => {
-    return (!!a) && (a.constructor === Object);
-};
+  });
+}
